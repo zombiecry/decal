373c8a85682f868657743463a3a7d98b22dcb5cb
zombiecry <yangsc2009@163.com>
2016-02-21 20:18:00 +0800

    new proj

---
+++ Assets/decal/script/DecalController.cs
@@ -0,0 +1,149 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Rendering;
+public class DecalController : MonoBehaviour {
+    Camera mDepthCamera;
+    private int cameraRightId;
+    private int cameraUpId;
+    private int cameraWorldPosId;
+    private int cameraNearLeftBottomCornerPointId;
+    private int cameraNearSizeId;
+    private int cameraToWorldMatrix;
+
+    public GameObject               DebugDrawSphere;
+    private CommandBuffer           commandBuf;
+    // Use this for initialization
+    void Awake()
+    {
+        mDepthCamera = Camera.main;
+        mDepthCamera.depthTextureMode |= DepthTextureMode.Depth;
+    }
+    void Start()
+    {
+        CreateMaterialsIfNeeded();
+        InitShaderProperties();
+        commandBuf = new CommandBuffer();
+        commandBuf.name = "draw decal";
+        mDepthCamera.AddCommandBuffer(CameraEvent.AfterEverything, commandBuf);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        CreateMaterialsIfNeeded();
+        DrawDecal();
+    }
+    private void CreateMaterialsIfNeeded()
+    {
+        Renderer renderer = GetRenderer();
+        if (renderer == null)
+        {
+            return;
+        }
+        if (renderer.materials == null)
+        {
+            renderer.materials = new Material[1];
+            renderer.materials[0] = LoadDecalMaterial();
+            return;
+        }
+        bool found = false;
+        for (int i = 0; i < renderer.materials.Length; i++)
+        {
+            if (renderer.materials[i] == null)
+            {
+                found = true;
+                renderer.materials[i] = LoadDecalMaterial();
+                break;
+            }
+            if (renderer.materials[i].name.StartsWith("BranchDemo/Decal"))
+            {
+                found = true;
+                break;
+            }
+        }
+        if (found)
+        {
+            return;
+        }
+        Material[] newMats = new Material[renderer.materials.Length + 1];
+        renderer.materials.CopyTo(newMats, 0);
+        newMats[newMats.Length - 1] = LoadDecalMaterial();
+        renderer.materials = newMats;
+    }
+    private void DrawDecal()
+    {
+        Renderer renderer = GetRenderer();
+        if (renderer == null)
+        {
+            return;
+        }
+        if (renderer.sharedMaterials != null)
+        {
+            for (int i = 0; i < renderer.materials.Length; i++)
+            {
+                if (renderer.materials[i] != null)
+                {
+                    if (renderer.materials[i].name.StartsWith("BranchDemo/Decal"))
+                    {
+                        UpdateShaderProperties(renderer.materials[i]);
+                        commandBuf.Clear();
+                        commandBuf.DrawRenderer(renderer, renderer.materials[i]);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    private Renderer GetRenderer()
+    {
+        Renderer renderer = GetComponent<SkinnedMeshRenderer>();
+        if (renderer == null)
+        {
+            renderer = GetComponent<Renderer>();
+        }
+        return renderer;
+    }
+    private Material LoadDecalMaterial()
+    {
+        return new Material(Shader.Find("BranchDemo/Decal"));
+    }
+    void InitShaderProperties()
+    {
+        cameraRightId = Shader.PropertyToID("CameraRight");
+        cameraUpId = Shader.PropertyToID("CameraUp");
+        cameraWorldPosId = Shader.PropertyToID("CameraWorldPos");
+        cameraNearLeftBottomCornerPointId = Shader.PropertyToID("CameraNearLeftBottomCornerPoint");
+        cameraNearSizeId = Shader.PropertyToID("CameraNearSize");
+        cameraToWorldMatrix = Shader.PropertyToID("CameraToWorld");
+    }
+    void UpdateShaderProperties(Material decalMat)
+    {
+        Camera camera = Camera.main;
+        Vector3 cameraPos = camera.transform.position;
+        float fov = camera.fieldOfView;
+        //local space xyz
+        //          y
+        //          |              
+        //          |              
+        //   camPos ------z
+        //          \
+        //           \
+        //            x
+        float z = camera.nearClipPlane;
+        float y = z * Mathf.Tan(fov * Mathf.Deg2Rad);
+        float x = y * ((float)Screen.width / (float)Screen.height);
+        Vector3 cameraNearLeftBottomCornerPoint = Vector3.zero;
+        cameraNearLeftBottomCornerPoint = cameraPos + z * camera.transform.forward - x * camera.transform.right - y * camera.transform.up;
+        decalMat.SetVector(cameraRightId, camera.transform.right);
+        decalMat.SetVector(cameraUpId, camera.transform.up);
+        decalMat.SetVector(cameraWorldPosId, camera.transform.position);
+        decalMat.SetVector(cameraNearLeftBottomCornerPointId, cameraNearLeftBottomCornerPoint);
+        decalMat.SetVector(cameraNearSizeId, new Vector2(x*2,y*2));
+        /*
+                float2 offset = depthUV * CameraNearSize.xy;
+				float3 nearPlanePos = CameraNearLeftBottomCornerPoint.xyz + offset.x * CameraRight.xyz + offset.y * CameraUp.xyz;
+        */
+        Vector2 offset = new Vector2(0.5f, 0.5f);
+        decalMat.SetMatrix(cameraToWorldMatrix, camera.cameraToWorldMatrix);
+        //DebugDrawSphere.transform.position = cameraNearLeftBottomCornerPoint+ offset.x * camera.transform.right+ offset.y * camera.transform.up;
+    }
+}
---
+++ Assets/decal/shader/Decal.shader
@@ -0,0 +1,83 @@
+﻿Shader "BranchDemo/Decal"
+{
+	Properties
+	{
+		_OcclusionBias ("OcclusionBias", Range(0.01, 10)) = 2.0
+		_BlendColor("BlendColor ",Color)= (1,1,1,0.2)
+	}
+	SubShader
+	{ 
+		Tags { "Queue" = "Transparent" "IgnoreProjector"="True" "RenderType"="Transparent"}
+		LOD 200
+		Pass
+		{
+			Blend SrcAlpha OneMinusSrcAlpha
+            Cull Off
+            ZWrite Off
+            //ZTest always
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+            uniform sampler2D _CameraDepthTexture; //Depth Texture
+            //camera coordinates
+            uniform float4 	  CameraRight;
+            uniform float4 	  CameraUp;
+            uniform float4 	  CameraWorldPos;
+            uniform float4 	  CameraNearLeftBottomCornerPoint;
+            uniform float4 	  CameraNearSize;
+            //------------------------------------
+
+			struct appdata
+			{
+				float4 pos 	 : POSITION;
+			};
+			struct v2f
+			{
+				float4 pos 		: POSITION;
+				float4 uv        : TEXCOORD0;
+				float4 objPos	: TEXCOORD1;
+			};
+			#include "UnityCG.cginc"
+			v2f vert (appdata v)
+			{ 
+				v2f o;
+				UNITY_INITIALIZE_OUTPUT(v2f,o);
+				o.pos = mul(UNITY_MATRIX_MVP, v.pos);
+				o.uv =  ComputeScreenPos(o.pos);
+				o.uv.z  =  -mul( UNITY_MATRIX_MV, v.pos ).z;
+				o.objPos = v.pos;
+				return o;
+			}
+			//viewz is simply the dist from the point to camera.
+			inline float3 CalcWorldSpacePos(float2 depthUV,float viewZ)
+			{
+				//make the depth uv start at left botton
+				//depthUV = (depthUV - 0.5) * float2(1,_ProjectionParams.x)+0.5;
+				float2 offset = depthUV * CameraNearSize.xy;
+				float3 nearPlanePos = CameraNearLeftBottomCornerPoint.xyz + offset.x * CameraRight.xyz + offset.y * CameraUp.xyz;
+				float3 dirFromCamera = normalize(nearPlanePos - CameraWorldPos.xyz);
+				return CameraWorldPos.xyz + dirFromCamera * viewZ * _ProjectionParams.z;
+			}
+			half4 frag (v2f i): COLOR
+			{
+				float2 depthUV = i.uv.xy/i.uv.w; 
+				float  zFromDepthMap=Linear01Depth(SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture,depthUV));
+				half   zFromCurrentFrag = i.uv.z;		//just for debug
+				float3 worldPos = CalcWorldSpacePos(depthUV,zFromDepthMap);
+				float3 objPos = mul(_World2Object, float4(worldPos,1)).xyz;
+				//objPos = i.objPos;
+				clip (float3(0.5,0.5,0.5) - abs(objPos.xyz));
+				//objPos =max(ceil(0.5 - abs(objPos)),float3(0,0,0));
+				half4 col = 1;
+				//col.a = objPos.x * objPos.y * objPos.z;
+				//col.a=1;
+				//col.rgb = objPos.y+0.5;
+				col.rgb = objPos.y+0.5;
+
+				return col;
+			}
+			ENDCG
+		}
+	}
+	Fallback Off
+}
